c3f51e904c0d9027fcbe126bd0b9c1af561da9e4
Braden Everson <bradeneverson@gmail.com>
2020-08-16 00:02:05 -0500

    Finalized return issue with going to current classroom

--- CovidClassroom/Pages/Classenvironment.cshtml.cs
+++ CovidClassroom/Pages/Classenvironment.cshtml.cs
@@ -11,34 +11,25 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CovidClassroom
 {
-    public class ClassroomModel : PageModel
+    public class ClassenvironmentModel : PageModel
     {
+        public Classroom currentClass;
         public readonly IClassRoomData classes;
         public readonly IStudentData students;
         public readonly ITeacherData teachers;
         public readonly ApplicationDbContext db;
         public IdentityUser currentUser = new IdentityUser();
-        public Classroom currentClass;
-        public ClassroomModel(ApplicationDbContext db, IStudentData students, ITeacherData teachers, IClassRoomData classes)
+        public ClassenvironmentModel(ApplicationDbContext db, IStudentData students, ITeacherData teachers, IClassRoomData classes)
         {
             this.db = db;
             this.students = students;
             this.teachers = teachers;
             this.classes = classes;
         }
-        public IActionResult OnGet(string classId)
+        public void OnGet(string classId)
         {
             currentUser = db.Users.FirstOrDefault(r => r.Email == User.Identity.Name);
             currentClass = classes.getByGuid(classId);
-            if (currentClass == null)
-            {
-                return RedirectToPage("./index");
-            }
-            else if (currentClass.students.FirstOrDefault(r => r.Email == currentUser.Email) == null)
-            {
-                return RedirectToPage("./index");
-            }
-            return Page();
         }
     }
 }
